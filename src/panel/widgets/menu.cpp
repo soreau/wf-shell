90ef38852823550a85c78eb4af6995cc1824f27f
Scott Moreau <oreaus@gmail.com>
2018-12-23 12:59:16 -0700

    Revert "menu: Set text length so themes have the same columns"
    This reverts commit 873eb9faaef653bd76d11633821d08d95fb2fec0
    because it is a HACK.

--- src/panel/widgets/menu.cpp
+++ src/panel/widgets/menu.cpp
@@ -57,8 +57,8 @@ void WayfireMenu::load_menu_item(std::string file)
             GTK_ICON_SIZE_LARGE_TOOLBAR);
         image->set_pixel_size(48);
         button->set_tooltip_text(name);
-        if (strlen(name) > 11)
-            strcpy(&name[9], "..");
+        if (strlen(name) > 12)
+            strcpy(&name[10], "..");
         label->set_text(name);
         button_box->add(*image);
         button_box->add(*label);
